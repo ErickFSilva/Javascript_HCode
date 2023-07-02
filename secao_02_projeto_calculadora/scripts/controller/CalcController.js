345296224b0d35a813a324c4db136f9f2ccbae6f
ErickFSilva <erick_ferreira@outlook.com>
2023-07-02 08:27:49 -0300

    Seção02: Projeto calculadora, 02/07/2023(manhã)

--- secao_02_projeto_calculadora/scripts/controller/CalcController.js
+++ secao_02_projeto_calculadora/scripts/controller/CalcController.js
@@ -4,18 +4,20 @@ class CalcController {
     constructor() {
 
         // Atributos:
+        this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
 
     // - Methods:
 
-    // initialize()
+    // initialize():
     initialize() {
 
         this.setDisplayDateTime();
@@ -27,15 +29,145 @@ class CalcController {
     }
 
 
-    // setDisplayDateTime()
+    // addEventListenerAll() - Método para o "click" ou o "arraste" no botão:
+    addEventListenerAll(element, events, fn) {
+
+        // Separa e converte o texto recebido, dos eventos, e converte-os em array.
+        events.split(" ").forEach(event => { // "event": representa o evento recebido. Sendo eles "click" ou "drag".
+
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+
+    // clearAll()
+    clearAll() {
+
+        this._operation = [];
+    }
+
+
+    // clearEntry()
+    clearEntry() {
+
+        this._operation.pop();
+    }
+
+
+    // addOperation()
+    addOperation(value) {
+
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+
+    // setError()
+    setError() {
+
+        this.displayCalc = "Error";
+    }
+
+
+    // execBtn()
+    execBtn(value) {
+
+        switch (value) {
+
+            // AC
+            case "ac":
+                this.clearAll();
+                break;
+
+            // CE
+            case "ce":
+                this.clearEntry();
+                break;
+
+            // SOMA
+            case "soma":
+                // 
+                break;
+
+            // SUBTRAÇÃO
+            case "subtracao":
+                // 
+                break;
+
+            // DIVISÃO
+            case "divisao":
+                // 
+                break;
+
+            // MILTIPLICAÇÃO
+            case "multiplicacao":
+                // 
+                break;
+
+            // PORCENTO
+            case "porcento":
+                // 
+                break;
+
+            // IGUAL
+            case "igual":
+                // 
+                break;
+
+            // NÚMEROS
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                this.addOperation(parseInt(value));
+                break;
+
+            // DEFAULT
+            default:
+                this.setError();
+                break;
+        }
+    };
+
+
+    // Iniciando os eventos dos botões: initButtonsEvents():
+    initButtonsEvents() {
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        // O "forEach" será utilizado para o reconhecimento de cada botão, da calculadora, no evento de click.
+        buttons.forEach((btn, index) => { // "btn": nome utilizado para o reconhecimento de cada ítem percorrido.
+
+            this.addEventListenerAll(btn, "click drag", e => {
+
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn();
+            })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
+            });
+        });
+    }
+
+
+    // setDisplayDateTime():
     setDisplayDateTime() {
 
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {day: '2-digit', month: 'long', year: 'numeric'});
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, { day: "2-digit", month: "long", year: "numeric" });
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
 
-    // Getter and Setter - displayDate()
+    // Getter and Setter - displayDate():
     get displayDate() {
 
         return this._dateEl.innerHTML;
@@ -47,7 +179,7 @@ class CalcController {
     }
 
 
-    // Getter and Setter - displayTime()
+    // Getter and Setter - displayTime():
     get displayTime() {
 
         return this._timeEl.innerHTML;
@@ -59,7 +191,7 @@ class CalcController {
     }
 
 
-    // Getter and Setter - displayCalc()
+    // Getter and Setter - displayCalc():
     get displayCalc() {
 
         return this._displayCalcEl.innerHTML;
@@ -71,7 +203,7 @@ class CalcController {
     }
 
 
-    // Getter and setter - currentDate()
+    // Getter and setter - currentDate():
     get currentDate() {
 
         return new Date();
